0f1d4a9116677e2cfe9a47723b6e22b5b2b407ce
Mohammed Sadaqathullah <mdsadaq.fed@gmail.com>
2025-06-08 12:22:23 +0000

    api issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/grocery', require('./routes/grocery'));
 app.use('/food', require('./routes/food'));
 app.use('/vegetables-and-fruits', require('./routes/vegetablesAndFruits'));
 app.use('/address', require('./routes/address'));
-app.use('/toyboxz', require('./models/ToyBoxz'));
+app.use('/toyboxz', require('./routes/toyboxz'));
 
 // Basic Route
 app.get('/', (req, res) => res.send('Hello, Backend is running!'));
