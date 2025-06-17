c133b77e40d5fb6329aa45f2663cdf4bb200ded6
Mohammed Sadaqathullah <mdsadaq.fed@gmail.com>
2025-06-17 12:24:10 +0000

    import issue

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const CryptoJS = require("../node_modules/crypto-js");
+const CryptoJS = require("crypto-js");
 
 
 router.post('/', async (req, res) => {
